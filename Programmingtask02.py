071cd48881cb320c0aa36cc574c910836ec4bb42
VREDDY0026 <vikranth.reddydodla@dal.ca>
2017-10-02 12:41:38 -0300

    doing

---
+++ Programmingtask02.py
@@ -0,0 +1,12 @@
+import csv
+
+
+my_file = 'crime.csv'
+
+
+def import_data(delimited_file):
+    with open(delimited_file, 'rb') as csvfile:
+        all_data = list(csv.reader(csvfile, delimiter=','))
+    return all_data
+
+print import_data(my_file)
